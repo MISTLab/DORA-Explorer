e7928f0fccc4bcdf1666bf7c83aa0980453f711e
DVielfaure <david.vielfaure@polymtl.ca>
2021-04-27 12:33:18 -0400

    Create heatmap from the results

--- sim/benchmarking/compare_radiation_map.py
+++ sim/benchmarking/compare_radiation_map.py
@@ -1,56 +1,84 @@
 ###
-# Comparison between the radiation beacons positions and the data collected in
-# the DBM
+# Creation of a heatmap of the belief map contructed by DORA
 ###
 
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import json
+import math
+from os import listdir
+from os.path import isfile, join
+import matplotlib
+
+#matplotlib.use('qt5Agg')
+
+matplotlib.use('Agg')
 
 ### Parameters
-result_file = "../results/result.txt"
-radiation_sources_file = "../data/radiation_sources.json"
+result_folder = "../results/gradient-final/"
+result_random_final_folder = "../results/random-walk-final/"
+radiation_sources_folder = "../data/"
+figures_folder = "figures/"
+number_of_steps_max = 100
+map_size = 50
+folders = [result_random_final_folder, result_folder]
 ###
 
-# Initialize figure
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+onlyfiles0 = [f for f in listdir(result_folder) if isfile(join(result_folder, f))]
+onlyfiles1 = [f for f in listdir(result_folder) if isfile(join(result_folder, f))]
+number_of_runs = int(min(len(onlyfiles0)/2, len(onlyfiles1)/2))
+number_of_folders = len(folders)
 
-# Read radiation results.
-result_X = np.array([])
-result_Y = np.array([])
-result_Z = np.array([])
-result_W = np.array([])
-f = open(result_file, "r")
-lines = f.readlines()
-for line in lines:
-    elems = line.split()
-    result_X = np.append(result_X,int(elems[0]))
-    result_Y = np.append(result_Y,int(elems[1]))
-    result_Z = np.append(result_Z,float(elems[2]))
-    result_W = np.append(result_W,float(elems[3]))
+for folder in range(0, number_of_folders):
+    print("---Processing folder " + folders[folder] + "---")
+    for run in range(0, number_of_runs):
+        #Files names
+        result_file = folders[folder] + "result" + str(run) + ".csv"
+        radiation_sources_file = radiation_sources_folder + "radiation_sources" + str(run) + ".json"
+        print("------- RUN " + str(run) + " -------")
+        
+        # Read radiation results.
+        f = open(result_file, "r")
+        result_X = np.array([])
+        result_Y = np.array([])
+        result_belief = np.array([])
+        lines = f.readlines()
+        for line in lines:
+            elems = line.split(',')
+            result_X = np.append(result_X, int(elems[0]))
+            result_Y = np.append(result_Y, int(elems[1]))
+            result_belief = np.append(result_belief, float(elems[2]))
 
-# Plot the results.
-results_points = ax.scatter(result_X,result_Y,result_Z, color="blue")
+        # Read the radiation sources
+        radiation_X = np.array([])
+        radiation_Y = np.array([])
+        radiation_intensity = np.array([])
+        with open(radiation_sources_file) as json_file:
+            data = json.load(json_file)
+            for r in data["sources"]:
+                radiation_X = np.append(radiation_X, float(r['x']))
+                radiation_Y = np.append(radiation_Y, float(r['y']))
+                radiation_intensity = np.append(
+                    radiation_intensity, float(r['intensity']))
+                
+        #Plot results
+        belief_map = np.zeros((map_size,map_size))
+        for i in range(len(result_belief)):
+            belief_map[int(result_Y[i]+ map_size/2 -1), int(result_X[i]+ map_size/2 -1)] = result_belief[i]
 
-# Read the radiation sources
-radiation_X = np.array([])
-radiation_Y = np.array([])
-radiation_Z = np.array([])
-with open(radiation_sources_file) as json_file:
-    data = json.load(json_file)
-    for r in data:
-        radiation_X = np.append(radiation_X,float(r['x']))
-        radiation_Y = np.append(radiation_Y,float(r['y']))
-        radiation_Z = np.append(radiation_Z,float(r['intensity']))
+        plot = sns.heatmap(belief_map, xticklabels=5, yticklabels=5, cmap="Blues")
+        plot.invert_yaxis()
+        plot.scatter(radiation_X + map_size/2 -1, radiation_Y + map_size/2 -1, marker='*', s=100, color='red')
+        
+        # Drawing the frame
+        for _, spine in plot.spines.items():
+            spine.set_visible(True)
+            spine.set_linewidth(1)
 
-# Plot the radiation sources
-radiation_points = ax.scatter(radiation_X,radiation_Y,radiation_Z, color="red")
+        #Save heatmap    
+        plot.figure.savefig(figures_folder + "heatmap_folder_" + str(folder) + "_run_" + str(run) + ".png", dpi=200)
+        plot.get_figure().clf() # this clears the figure
 
-# Show plot
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Radiation level")
-ax.set_zlim([0, 1])
-plt.show()
+            
