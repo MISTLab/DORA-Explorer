Fixed variance calculation

sim/benchmarking/compute_metrics.py
@@ -13,9 +13,9 @@ import matplotlib
 matplotlib.use('Agg')
 
 ### Parameters
-result_folder_frontier = "../results/frontier_15/"
-result_folder_random = "../results/randomwalk_15/"
-result_folder_dora = "../results/dora_15/"
+result_folder_frontier = "../results/frontier_5/"
+result_folder_random = "../results/randomwalk_5/"
+result_folder_dora = "../results/dora_5/"
 radiation_sources_folder = "../data/"
 figures_folder = "figures/"
 number_of_steps_max = 300
@@ -144,7 +144,7 @@ colors = ["lightcoral","orchid", "cornflowerblue"]
 fig = plt.figure()
 ax = fig.gca()
 for f in range(0, number_of_folders):
-    std = 0.5 * np.nanstd(number_active_robots_step[f, :, :])
+    std = np.array([0.5 * np.nanstd(number_active_robots_step[f, :, i]) for i in range(number_of_steps_max)])
     mean = number_active_robots_step[f, :, :].mean(0)
     ax.scatter(x_axis, mean, c=colors[f])
     ax.fill_between(x_axis, mean-std, mean+std, alpha=0.25, color=colors[f])
@@ -156,7 +156,7 @@ plt.savefig(figures_folder + "activerobots.png")
 fig = plt.figure()
 ax = fig.gca()
 for f in range(0, number_of_folders):
-    std = 0.5 * np.nanstd(number_of_cases_explored[f, :, :])
+    std = np.array([0.5 * np.nanstd(number_of_cases_explored[f, :, i]) for i in range(number_of_steps_max)])
     mean = number_of_cases_explored[f, :, :].mean(0)
     ax.scatter(x_axis, mean, c=colors[f])
     ax.fill_between(x_axis, mean-std, mean+std, alpha=0.25, color=colors[f])
@@ -168,7 +168,7 @@ plt.savefig(figures_folder + "explored.png")
 fig = plt.figure()
 ax = fig.gca()
 for f in range(0, number_of_folders):
-    std = 0.5 * np.nanstd(scaled_amount_of_radiation[f, :, :])
+    std = np.array([0.5 * np.nanstd(scaled_amount_of_radiation[f, :, i]) for i in range(number_of_steps_max)])
     mean = scaled_amount_of_radiation[f, :, :].mean(0)
     ax.scatter(x_axis, mean, c=colors[f])
     ax.fill_between(x_axis, mean-std, mean+std, alpha=0.25, color=colors[f])
@@ -180,7 +180,7 @@ plt.savefig(figures_folder + "radiation.png")
 fig = plt.figure()
 ax = fig.gca()
 for f in range(0, number_of_folders):
-    std = 0.5 * np.nanstd(average_belief_error[f, :, :])
+    std = np.array([0.5 * np.nanstd(average_belief_error[f, :, i]) for i in range(number_of_steps_max)])
     mean = average_belief_error[f, :, :].mean(0)
     ax.scatter(x_axis, mean, c=colors[f])
     ax.fill_between(x_axis, mean-std, mean+std, alpha=0.25, color=colors[f])
@@ -192,7 +192,7 @@ plt.savefig(figures_folder + "error.png")
 fig = plt.figure()
 ax = fig.gca()
 for f in range(0, number_of_folders):
-    std = 0.5 * np.nanstd(amount_transmitted[f, :, :])/1000.0
+    std = np.array([0.5 * np.nanstd(amount_transmitted[f, :, i]) for i in range(number_of_steps_max)])/1000.0
     mean = amount_transmitted[f, :, :].mean(0)/1000.0
     ax.scatter(x_axis, mean, c=colors[f])
     ax.fill_between(x_axis, mean-std, mean+std, alpha=0.25, color=colors[f])